77bf8db46adec68dda182787a5f290cbb821d826
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2021-03-19 20:37:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const toggleButton = document.querySelectorAll('.faq-toggle');
+
+toggleButton.forEach((button) =>{
+    button.addEventListener('click',()=>{
+        button.parentNode.classList.toggle('active');
+    })
+})
+
